88ff98d08cf10985f3cd9b624c6a2bf654254c4d
jadon <16850875+wolffshots@users.noreply.github.com>
2022-07-07 23:06:50 +0200

    feat: updated clap package and refactored code to practice

--- src/main.rs
+++ src/main.rs
@@ -2,77 +2,117 @@ extern crate clap;
 extern crate serialport;
 
 use std::io::{self, Write};
+use std::ops::RangeInclusive;
 use std::time::Duration;
 
-use clap::{App, AppSettings, Arg, SubCommand};
+use clap::{Arg, Command};
+
+const BAUD_DEFAULT: &str = "115200";
+const BAUD_RANGE: RangeInclusive<usize> = 50..=921600;
 
 /**
  * entrypoint for app
  */
 fn main() {
-    let matches = App::new("Recieve input from a serial device")
-        .about("Reads data from a serial port and echoes it to stdout")
-        .setting(AppSettings::DisableVersion)
-        .setting(AppSettings::ArgRequiredElseHelp)
-        .setting(AppSettings::ColoredHelp)
+    let command = Command::new("wolffserial")
+        .about("a command line tool for working with serial devices")
+        .disable_version_flag(true)
+        .arg_required_else_help(true)
         .subcommand(
-            SubCommand::with_name("watch")
-                .about("Watch a specific device")
+            Command::new("watch")
+                .about("watch a specific device")
                 .arg(
                     Arg::with_name("port")
-                        .help("The device path to a serial port")
-                        .use_delimiter(false)
+                        .help("the device path to a serial port")
+                        .use_value_delimiter(false)
                         .required(true),
                 )
                 .arg(
                     Arg::with_name("baud")
-                        .help("The baud rate to connect at")
-                        .use_delimiter(false)
+                        .help("the baud rate to connect at")
+                        .use_value_delimiter(false)
                         .required(false)
-                        .validator(valid_baud)
-                        .default_value("115200"),
+                        .value_parser(baud_in_range)
+                         .default_value(BAUD_DEFAULT),
                 ),
         )
-        .subcommand(SubCommand::with_name("list").about("Lists available devices"))
-        .get_matches();
-    if let Some(matches) = matches.subcommand_matches("watch") {
-        let port_name = matches.value_of("port").unwrap();
-        let baud_rate = matches.value_of("baud").unwrap().parse::<u32>().unwrap();
-        let port = serialport::new(port_name, baud_rate)
-            .timeout(Duration::from_millis(100))
-            .open();
-        match port {
-            Ok(mut port) => {
-                let mut serial_buf: Vec<u8> = vec![0; 1000];
-                println!("Receiving data on {} at {} baud:", &port_name, &baud_rate);
-                loop {
-                    match port.read(serial_buf.as_mut_slice()) {
-                        Ok(t) => io::stdout().write_all(&serial_buf[..t]).unwrap(),
-                        Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
-                        Err(e) => {
-                            eprintln!("{:?}", e);
-                            ::std::process::exit(1);
-                        },
+        .subcommand(
+            Command::new("list")
+                .about("lists available devices")
+                .long_about(
+                    "shows the number of connected/detected serial devices as well as a list of devices, \
+                    what their paths/names are and what type of device they are detected as"
+                )
+        );
+
+    let matches = command.get_matches();
+
+    match matches.subcommand() {
+        Some(("watch", matches)) => {
+            // matches here is shadowing the matches defined above
+            let port_name = match matches.get_one::<String>("port") {
+                Some(val) => val,
+                None => {
+                    println!("port is required");
+                    ::std::process::exit(0);
+                }
+            };
+            let baud: u32 = match matches.get_one::<u32>("baud") {
+                Some(baud) => {
+                    // not the most concise way of getting the value out of an Option
+                    *baud // but i am practicing matching
+                }
+                _ => unreachable!(),
+            };
+            let port = serialport::new(port_name, baud)
+                .timeout(Duration::from_millis(100))
+                .open();
+            match port {
+                Ok(mut port) => {
+                    let mut serial_buf: Vec<u8> = vec![0; 1000];
+                    println!("Receiving data on {} at {} baud:", &port_name, &baud);
+                    loop {
+                        match port.read(serial_buf.as_mut_slice()) {
+                            Ok(t) => io::stdout().write_all(&serial_buf[..t]).unwrap(),
+                            Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
+                            Err(e) => {
+                                eprintln!("{:?}", e);
+                                ::std::process::exit(1);
+                            }
+                        }
                     }
                 }
-            }
-            Err(e) => {
-                eprintln!("Failed to open \"{}\". Error: {}", port_name, e);
-                ::std::process::exit(1);
+                Err(e) => {
+                    eprintln!("Failed to open \"{}\". Error: {}", port_name, e);
+                    ::std::process::exit(1);
+                }
             }
         }
-    } else if let Some(_sub_matches) = matches.subcommand_matches("list") {
-        list_ports();
+        Some(("list", _)) => {
+            list_ports();
+        }
+        None => {
+            println!("no matching subcommand found");
+            ::std::process::exit(0);
+        }
+        _ => unreachable!(),
     }
 }
 
 /**
- * validate that the baudrate is an unsigned 32 bit number
- */
-fn valid_baud(val: String) -> Result<(), String> {
-    val.parse::<u32>()
-        .map(|_| ())
-        .map_err(|_| format!("Invalid baud rate '{}' specified", val))
+	* value parser function to make sure baud is a number and in the range we expect
+	*/
+fn baud_in_range(s: &str) -> Result<u32, String> {
+    let port: usize = s.parse().map_err(|_| format!("`{}` isn't a number", s))?;
+    if BAUD_RANGE.contains(&port) {
+        Ok(port as u32)
+    } else {
+        Err(format!(
+            "baud not in range {}-{}, if this is an error please post an issue @ github.com/wolffshots/wolffserial-rs/issues",
+            BAUD_RANGE.start(),
+            BAUD_RANGE.end()
+        ))
+    }
 }
 
 /**
@@ -94,10 +134,10 @@ fn list_ports() {
  * returns a String with a brief decriptor for the port
  */
 fn get_port_type(port_type: serialport::SerialPortType) -> String {
-    return match port_type{
-        serialport::SerialPortType::UsbPort(_) => {String::from("usb")}
-        serialport::SerialPortType::PciPort => {String::from("pci")}
-        serialport::SerialPortType::BluetoothPort => {String::from("bluetooth")}
-       _ => {String::from("unknown")}
+    return match port_type {
+        serialport::SerialPortType::UsbPort(_) => String::from("usb"),
+        serialport::SerialPortType::PciPort => String::from("pci"),
+        serialport::SerialPortType::BluetoothPort => String::from("bluetooth"),
+        _ => String::from("unknown"),
     };
 }
